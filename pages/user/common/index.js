01888b748ebe3d80d533ad3db380315216ad7a03
guo2006cd <326605237@qq.com>
2023-04-20 23:05:24 +0800

    展示积分商城商品描述

--- pages/user/common/index.js
+++ pages/user/common/index.js
@@ -19,3 +19,47 @@ export const formatTime = (number, format)=> {
   })
   return format;
 } 
+
+  // 根据生日获取年龄
+  export const getAge = (s) =>{
+  if(s == '' || s == null || s== 'null') return ''
+  let returnAge;
+  let d1= (s).replace(/\-/g, '/')
+  const birthday = new Date(d1)
+  const birthYear = birthday.getFullYear();
+  const birthMonth = birthday.getMonth() + 1;
+  const birthDay = birthday.getDate() + 1;
+ 
+  const d = new Date();
+  const nowYear = d.getFullYear();
+  const nowMonth = d.getMonth() + 1;
+  const nowDay = d.getDate();
+  if(nowYear == birthYear){
+    returnAge = 0;//同年 则为0岁
+  }
+  else{
+    const ageDiff = nowYear - birthYear ; //年之差
+    if(ageDiff > 0){
+      if(nowMonth == birthMonth) {
+        var dayDiff = nowDay - birthDay;//日之差
+        if(dayDiff < 0){
+          returnAge = ageDiff - 1;
+        }else{
+          returnAge = ageDiff ;
+        }
+      }else{
+        var monthDiff = nowMonth - birthMonth;//月之差
+        if(monthDiff < 0){
+          returnAge = ageDiff - 1;
+        }else{
+          returnAge = ageDiff ;
+        }
+      }
+    }
+    else
+    {
+      returnAge = -1;//出生日期不能大于今天
+    }
+  }
+  return returnAge;
+}
